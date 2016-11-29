6632b923a18476d2642b1f2473e10a13adf45a23
Jesse Zhang <jesse.zhang@tapad.com>
2016-11-29 10:40:21 -0500

    add credits

--- highcharts.d.ts
+++ highcharts.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for Highcharts 4.2.5
+// Project: http://www.highcharts.com/
+// Definitions by: Damiano Gambarotto <http://github.com/damianog>, Dan Lewi Harkestad <http://github.com/baltie>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 // Original typings from: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/highcharts/highcharts.d.ts
 
 interface Position {
